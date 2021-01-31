09d33dcbb53f50f6a72f0719679f07f7a70c031f
ruben <ruben@DESKTOP-EI4MDPD>
2021-01-31 19:12:38 +0100

    cambio2cambios

---
+++ actividad1/src/actividad1/clase2RamaCambios.java
@@ -0,0 +1,11 @@
+package actividad1;
+
+public class clase2RamaCambios {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Esta es la segunda clase de la rama cmabios");
+		
+	}
+
+}
---
+++ actividad1/src/actividad1/claseRamaCambios.java
@@ -0,0 +1,11 @@
+package actividad1;
+
+public class claseRamaCambios {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Esta clase pertenece a la nueva rama cambios");
+		
+	}
+
+}
